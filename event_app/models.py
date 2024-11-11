e295f821cf146ae6a0894712f492615398f07b61
Taisiia Simakova <stipaxa@gmail.com>
2024-11-11 22:01:06 +0000

    Add WoofspotEvent model

--- event_app/models.py
+++ event_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class WoofspotEvent(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    location = models.CharField(max_length=200, unique=True)
+    event_date = models.DateField()
+    event_time = models.TimeField()
+    duration = models.DurationField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True) 
+
+    def __str__(self):
+        return f"Event: {self.title} at {self.location} on {self.event_date}"
+
+    class Meta:
+        ordering = ["-event_date", "event_time"]
+        verbose_name = "Pet-Friendly Event"
