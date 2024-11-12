95916fa3b1c7224a562308285b60fb1c2d796bfd
Taisiia Simakova <stipaxa@gmail.com>
2024-11-12 18:04:18 +0000

    feat: Refactor WoofspotEvent model
    - Renamed event_time to event_start_time and added event_end_time.
    - Implemented clean() for validation and save() for slug generation.

--- event_app/models.py
+++ event_app/models.py
@@ -1,20 +1,41 @@
 from django.db import models
+from django.utils.text import slugify
+from django.core.exceptions import ValidationError
+from datetime import datetime, date
 
 # Create your models here.
 class WoofspotEvent(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
-    location = models.CharField(max_length=200, unique=True)
+    location = models.CharField(max_length=200)
     event_date = models.DateField()
-    event_time = models.TimeField()
-    duration = models.DurationField()
+    event_start_time = models.TimeField()
+    event_end_time = models.TimeField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True) 
 
     def __str__(self):
         return f"Event: {self.title} at {self.location} on {self.event_date}"
 
+    def clean(self):
+        # Validate that end time is after start time
+        if self.event_end_time <= self.event_start_time:
+            raise ValidationError("Event end time must be after the start time.")
+
+        if self.event_date < date.today() or (self.event_date == date.today() and 
+         self.event_start_time < datetime.now().time()):
+            raise ValidationError("Event cannot be in the past.")    
+
+        super().clean()
+
+    def save(self):
+        # Generate slug, perform validation and save the object
+        self.full_clean()
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save()
+
     class Meta:
-        ordering = ["-event_date", "event_time"]
+        ordering = ["-event_date", "event_start_time"]
         verbose_name = "Pet-Friendly Event"
