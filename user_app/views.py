fc0a6d3199507d16253c381dacf442b8be9e3674
Taisiia Simakova <stipaxa@gmail.com>
2024-11-15 17:03:23 +0000

    feat: Add website account signup feature

--- user_app/views.py
+++ user_app/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login_new_page(request):
-    print(request)
     return render(request, "user_app/login_new.html")
-    # return HttpResponse("Hello from login new page!")
