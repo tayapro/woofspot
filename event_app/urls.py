96048309104dc6771fdd5db375eb98eaf53403f9
Taisiia Simakova <stipaxa@gmail.com>
2024-11-17 21:30:53 +0000

    feat: Restrict event reservation
    Only authenticated users able to make reservations.

--- event_app/urls.py
+++ event_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.FetchEvents.as_view(), name='home'),
-    path('<slug:slug>/', views.get_event, name="event-detail"),
+    path('<slug:slug>/', views.event_detail, name="event-detail"),
 ]
--- event_app/views.py
+++ event_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.views import generic
+from django.contrib import messages
 from .models import WoofspotEvent
 
 # Create your views here.
@@ -10,8 +12,20 @@ class FetchEvents(generic.ListView):
     paginate_by = 6
 
 
-def get_event(request, slug):
-    queryset = WoofspotEvent.objects.all()
-    event = get_object_or_404(queryset, slug=slug)
-    return render(request, "event_app/event_info.html", {"event": event},)
+def event_detail(request, slug):
+    event = get_object_or_404(WoofspotEvent, slug=slug)
 
+    if request.method == 'POST' and 'reserve_spot' in request.POST:
+        if request.user.is_authenticated:
+            if request.user in event.attendees.all():
+                messages.error(request, "You have already reserved a spot for this event.")
+            else:
+                event.attendees.add(request.user)
+                messages.success(request, "Slot is reserved!")
+        else:
+            return redirect(f"{reverse('login_new')}?next={request.path}")
+
+    user_registered = (
+        request.user.is_authenticated and request.user in event.attendees.all()
+    )
+    return render(request, "event_app/event_info.html", {"event": event, "user_registered": user_registered})
