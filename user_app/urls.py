02ce4da2dd2815df894b62781b01753fba6fe41f
Taisiia Simakova <stipaxa@gmail.com>
2024-11-22 20:29:53 +0000

    feat(UI): Improve get_started page structure and styles

--- user_app/urls.py
+++ user_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.get_started_page, name='get_started'),
-    path("logout_new/", views.logout_new_page, name='logout_new'),
+    path("get-started/", views.get_started_page, name='get_started'),
+    path("logout-new/", views.logout_new_page, name='logout_new'),
     # TODO: add url for profile 
 ]
