496fffa0cc2cb279eba7ef53c62d9cd02e95caa1
Taisiia Simakova <stipaxa@gmail.com>
2024-12-01 13:24:13 +0000

    refactor: Rename event_app url names

--- event_app/urls.py
+++ event_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.FetchEvents.as_view(), name="home"),
     path('myevents/', views.events_page, name="events"),
-    path('myevents/cancel/<slug:slug>/', views.cancel_event_page, name="cancel-event"),
-    path('<slug:slug>/', views.event_detail, name="event-detail"),
+    path('myevents/cancel/<slug:slug>/', views.cancel_event_page, name="cancel_event"),
+    path('<slug:slug>/', views.event_detail, name="event_detail"),
 ]
