51fc752b7ef90d81b89c4cf7cd51e449f02bc569
Taisiia Simakova <stipaxa@gmail.com>
2025-01-05 23:43:10 +0000

    revert: Roll back changes for allauth feature customizations
    Put on hold User Story: "Custom Templates for Password Management Pages"
    Reference: https://github.com/tayapro/woofspot/issues/32

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
-from allauth.account.views import PasswordResetView
-from user_app.forms import CustomResetPasswordForm
 from . import views
 
-
 urlpatterns = [
-    path("accounts/password/reset/",
-        PasswordResetView.as_view(form_class=CustomResetPasswordForm),
-        name="account_reset_password",
-    ),
     path("profile/", views.profile, name='profile'),
 ]
