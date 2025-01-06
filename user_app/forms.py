10978e398742156a76794952687c96a0677732e8
Taisiia Simakova <stipaxa@gmail.com>
2025-01-06 17:23:45 +0000

    feat: Simplify error list structure in forms
    - Updated error rendering to display a flat error list structure.
    - Replaced nested error `<ul>` elements with a single list for better readability.
    - Capitalized field names and joined multiple errors with commas for cleaner output.

--- user_app/forms.py
+++ user_app/forms.py
@@ -15,17 +15,6 @@ class MySigninForm(LoginForm):
         for field_name, field in self.fields.items():
             field.widget.attrs.update({**TEXT_STYLES})
     
-    def get_errors_as_single_list(self):
-        errors = []
-        # Collect field errors
-        for field, field_errors in self.errors.items():
-            for error in field_errors:
-                if field == "__all__":
-                    errors.append(f"{error}")
-                else:
-                    errors.append(f"{field.capitalize()}: {error}")
-        return errors
-
 
 class MySignupForm(SignupForm):
     def __init__(self, *args, **kwargs):
