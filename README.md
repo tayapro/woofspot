016e8658a2faca94e4a2028b2307181a40ffcf32
Taisiia Simakova <stipaxa@gmail.com>
2025-01-19 14:19:00 +0000

    docs: Add comments to event_app models and create README

---
+++ README.md
@@ -0,0 +1,59 @@
+
+## Table of Contents
+
+- [Project Description](#project-description)
+  - [Purpose](#purpose)
+  - [User Demographics](#user-demographics)
+- [UX Design](#ux-design)
+  - [User Stories](#user-stories)
+- [Features](#features)
+  - [Existing Features](#existing-features)
+  - [Future Features](#future-features)
+- [Technical Overview](#technical-overview)
+  - [Flowcharts](#flowcharts)
+  - [Data Model](#data-model)
+- [Technologies](#technologies)
+- [Deployment](#deployment)
+  - [Github](#github)
+  - [Heroku](#heroku)
+- [Testing](#testing)
+  - [User Stories Testing](#user-stories-testing)
+  - [Manual Testing](#manual-testing)
+  - [Unit Testing](#unit-testing)
+  - [Code Validation Testing](#code-validation-testing)
+  - [Known Issues](#known-issues)
+- [Credits](#credits)
+  - [Media](#media)
+  - [Code](#code)
+- [Acknowledgments](#acknowledgments)
+
+
+
+
+
+# Testing
+
+## User Stories Testing
+
+This section shows the connection between [Features](#features) and [UX design](#ux-design) sections.
+
+## Code Validation Testing
+
+### HTML
+
+The W3C Markup Validation Service was employed to check the HTML of the website.
+Only `layout.html` has been slightly changed.
+
+<details><summary><code>XXXXX.html</code> page passed without any errors or warnings.</summary>
+<img src="readme/W3HTML_validation_XXXXX.png" width="500" alt="W3C XXXXXX.html validation image">
+</details>
+
+### Python
+
+All files passed without any errors or warnings on [CI Python Linter](https://pep8ci.herokuapp.com/).
+
+#### event_app
+
+<details><summary><code>models.py</code> page passed without any errors or warnings.</summary>
+<img src="readme/PEP8_event_app_models.png" width="500" alt="PEP8 event_app models validation image">
+</details>
--- event_app/models.py
+++ event_app/models.py
@@ -10,16 +10,27 @@ from datetime import datetime, date, time
 from django.contrib.auth.models import User
 
 
-# Validate that the event date is not in the past
 def date_validation(value):
-    if value < date.today():
+    """
+    Ensure the event date is not set in the past.
+    Raises:
+        ValidationError: If the date is earlier than tomorrow.
+    """
+
+    if value <= date.today():
         raise ValidationError("Event date cannot be in the past.")
 
 
-# Validate event's image file
 def file_validation(file):
-    min_file_size = 20480 # 20kB
-    max_file_size = 1024 * 1024 * 2 # 2mb file
+    """
+    Validate the uploaded image file.
+    Ensures the file size and type meet specified requirements.
+    Raises:
+        ValidationError: If the file is invalid.
+    """
+
+    min_file_size = 20480  # Minimum file size: 20KB
+    max_file_size = 1024 * 1024 * 2  # Maximum file size: 2MB
     allowed_types = ['image/png', 'image/jpg', 'image/jpeg']
 
     if not file:
@@ -36,6 +47,11 @@ def file_validation(file):
 
 
 class WoofspotEvent(models.Model):
+    """
+    Represents an event with details like title, location, and time.
+    Allows for attendee and organizer tracking.
+    """
+
     title = models.CharField(max_length=55, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
@@ -43,30 +59,45 @@ class WoofspotEvent(models.Model):
     date = models.DateField(validators=[date_validation])
     start_time = models.TimeField()
     end_time = models.TimeField()
-    attendees = models.ManyToManyField(User, related_name="events_attending", blank=True)
+    attendees = models.ManyToManyField(User, related_name="events_attending",
+                                       blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    liked_by = models.ManyToManyField(User, related_name="liked_events", blank=True)
-    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="organized_events", default=1)
-    image = CloudinaryField('image', folder=settings.CLOUDINARY_UPLOAD_FOLDER, validators=[file_validation], resource_type='auto', blank=True)
+    liked_by = models.ManyToManyField(User, related_name="liked_events",
+                                      blank=True)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE,
+                                  related_name="organized_events", default=1)
+    image = CloudinaryField('image', folder=settings.CLOUDINARY_UPLOAD_FOLDER,
+                            validators=[file_validation],
+                            resource_type='auto', blank=True)
 
     def __str__(self):
+        """
+        Return a human-readable string representation of the event.
+        """
         return f"Event: {self.title} at {self.location} on {self.date}"
 
     def clean(self):
+        """
+        Perform model-level validation for event timing.
+        Ensures start and end times are valid and within
+        specified constraints.
+        """
+
         super().clean()
 
-        # Validate that end time is after start time
-        if self.date < date.today() or (self.date == date.today() and 
-          self.start_time < datetime.now().time()):
+        if self.date < date.today() or (self.date == date.today() and
+           self.start_time < datetime.now().time()):
             raise ValidationError("Event cannot be in the past")
         if self.end_time <= self.start_time:
-            raise ValidationError("Event end time must be after the start time")
+            raise ValidationError("Event end time must be after " +
+                                  "the start time")
 
         night_start = time(21, 0)
         night_end = time(9, 0)
         if self.start_time >= night_start or self.start_time < night_end:
-            raise ValidationError("Event start time cannot be between 21:00 and 09:00")
+            raise ValidationError("Event start time cannot be between " +
+                                  "21:00 and 09:00")
 
         start_datetime = datetime.combine(datetime.now(), self.start_time)
         end_datetime = datetime.combine(datetime.now(), self.end_time)
@@ -74,60 +105,107 @@ class WoofspotEvent(models.Model):
 
         # 10800 is 3 hours
         if time_difference.total_seconds() > 10800:
-            raise ValidationError("Please make sure the event is no longer than three hours")
+            raise ValidationError("Please make sure the event is no longer " +
+                                  "than three hours")
 
         if time_difference.total_seconds() < 3600:
             raise ValidationError("The minimum event duration is one hour")
 
-
     def save(self, *args, **kwargs):
-        # Generate slug, perform validation and save the object
+        """
+        Automatically generate a slug based on the title before saving.
+        """
+
         if not self.slug:
             self.slug = slugify(self.title)
-        self.full_clean()  
+        self.full_clean()
         super().save(*args, **kwargs)
 
-
     def like_toggle(self, user):
-        # Toggle the like status of a user
+        """
+        Toggle like status for the given user.
+        """
+
         if user in self.liked_by.all():
             self.liked_by.remove(user)
         else:
             self.liked_by.add(user)
 
     def like_count(self):
-        # Return the number of likes
+        """
+        Return the total number of likes for the event.
+        """
+
         return self.liked_by.count()
 
     def remove_image(self):
+        """
+        Remove the event image from Cloudinary and unset the image field.
+        """
+
         public_id = self.image.public_id
         destroy(public_id, invalidate=True)
         self.image = None
         self.save(update_fields=['image'])
 
     class Meta:
+        """
+        Metadata for the WoofspotEvent model.
+
+        - Default ordering by creation timestamp in descending order.
+        - Name for the admin panel.
+        """
         ordering = ["-date", "start_time"]
         verbose_name = "The Event"
 
 
-# 1 to 5 stars
+# Define choices for a 1 to 5 star rating
 RATING_CHOICES = [(i, f"{i} Star{'s' if i > 1 else ''}") for i in range(1, 6)]
 
+
 class Rating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_ratings')
-    event = models.ForeignKey(WoofspotEvent, on_delete=models.CASCADE, null=True, related_name='event_ratings')
+    """
+    Represents a user's rating and optional review for an event.
+    Includes metadata for creation and update timestamps.
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='user_ratings')
+    event = models.ForeignKey(WoofspotEvent, on_delete=models.CASCADE,
+                              null=True, related_name='event_ratings')
     rating = models.PositiveSmallIntegerField(choices=RATING_CHOICES)
     review_text = models.TextField(default="", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def get_average_rating(event):
+        """
+        Calculate the average rating for the specified event.
+        Parameters:
+            event: The event for which to calculate the average rating.
+        Returns:
+            float: The average rating for the event,
+                   or None if no ratings exist.
+        """
+
         return event.event_ratings.aggregate(Avg('rating'))['rating__avg']
 
     def __str__(self):
-        return f"Rating: {self.rating} stars by {self.user} for {self.event.title}"
+        """
+        Return a human-readable string representation of the rating.
+        """
+
+        return (f"Rating: {self.rating} stars by {self.user} for "
+                f"{self.event.title}")
 
     class Meta:
+        """
+        Metadata for the Rating model.
+        - Default ordering by creation timestamp in descending order.
+        - Database indexes on user and event fields for optimized querying.
+        - Name for the admin panel.
+        """
+
         ordering = ['-created_at']
         indexes = [
             models.Index(fields=['user']),
